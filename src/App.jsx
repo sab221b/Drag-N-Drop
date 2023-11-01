99df54d9c9352810d51d89e6fe6ce942cac223d7
sab221b <sab221b@gmail.com>
2023-11-01 16:55:44 +0530

    code optimisation

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,16 @@ function App() {
     <div className="container">
       <p>Drag & Drop Elements</p>
       <DraggableContainer>
-        {/* child - 1 */}
         <button className='btn btn-secondary'>{"Button"}</button>
-        {/* child - 2 */}
-        <div class="form-group col-10">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+        <div className="form-group col-10">
+          <label htmlFor="exampleInputEmail1">Email address</label>
+          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
         </div>
-        {/* child - 3 */}
         <h4 className='m-0'>This is a heading</h4>
-        {/* child - 4 */}
         <CustomComponent />
-        {/* child - 5 */}
-        <div class="form-check">
-          <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-          <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        <div className="form-check">
+          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
         </div>
       </DraggableContainer>
       <br />
