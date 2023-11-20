dfbf50bcc754cf8b61eb884bd412986f61ecd0b3
sab221b <sab221b@gmail.com>
2023-11-20 19:24:32 +0530

    add css & animation

--- src/DraggableContainer/DraggableContainer.jsx
+++ src/DraggableContainer/DraggableContainer.jsx
@@ -1,10 +1,10 @@
 import { useEffect } from 'react';
-import './DraggableContainer.css';
+import './DraggableContainer.scss';
 import { useState } from 'react';
 
-function DraggableContainer(props) {
+function DraggableContainer({ children }) {
 
-  const childrenComponents = props.children.map((item, index) => {
+  const childrenComponents = children.map((item, index) => {
     return {
       ...item,
       id: String(new Date().getTime() + index)
@@ -21,6 +21,8 @@ function DraggableContainer(props) {
   useEffect(() => {
     if (dragItem.item && dropItem.item && dragItem.index !== dropItem.index) {
       dragNdrop();
+    } else if (dragItem.item && dropItem.item && dragItem.index === dropItem.index) {
+      resetDragDrop();
     }
   }, [dragItem, dropItem])
 
@@ -29,8 +31,30 @@ function DraggableContainer(props) {
     setDropItem({});
   }
 
-  const onDrag = (item, index) => {
+  const onDragStart = (item, index, event) => {
+    event.target.style.opacity = '0.4';
+    event.dataTransfer.effectAllowed = 'move';
+    event?.dataTransfer.setData('text/html', event?.target.innerHTML);
     setDragItem({ item, index });
+  }
+
+  const onDragEnter = (event) => {
+    if(event.target.classList.contains('draggable')) {
+      event.target.classList.add('dragover');
+    }
+  }
+
+  const onDragLeave = (event) => {
+    event.stopPropagation();
+    event.target.classList.remove('dragover');
+  }
+
+  const onDragEnd = (event) => {
+    var listItems = document.querySelectorAll('.draggable');
+    [].forEach.call(listItems, function(item) {
+      item.classList.remove('dragover');
+    });
+    event.target.style.opacity = '1';
   }
 
   const onDrop = (item, index) => {
@@ -49,9 +73,12 @@ function DraggableContainer(props) {
       {list.map((item, index) =>
         <div
           key={item.id}
-          onDragStart={() => onDrag(item, index)}
+          className="draggable"
+          onDragStart={(event) => onDragStart(item, index, event)}
+          onDragEnter={onDragEnter}
+          onDragLeave={onDragLeave}
+          onDragEnd={onDragEnd}
           onDrop={() => onDrop(item, index)}
-          className="list-item"
           id={item.id}
           draggable={true}
         >{item}</div>
