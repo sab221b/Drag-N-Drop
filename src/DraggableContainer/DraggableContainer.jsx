92011037b44de4339c21e0b2f1dbe85c4c528197
sab221b <sab221b@gmail.com>
2024-06-28 17:38:20 +0530

    fix build error

--- src/DraggableContainer/DraggableContainer.jsx
+++ src/DraggableContainer/DraggableContainer.jsx
@@ -1,76 +1,76 @@
-import { useEffect } from 'react';
-import './DraggableContainer.scss';
-import { useState } from 'react';
+import { useEffect, useState, useCallback } from "react";
+import "./DraggableContainer.scss";
 
 function DraggableContainer({ children }) {
-
   const childrenComponents = children.map((item, index) => {
     return {
       ...item,
-      id: String(new Date().getTime() + index)
-    }
-  })
+      id: String(new Date().getTime() + index),
+    };
+  });
   const [list, setList] = useState(childrenComponents);
-  const [dragItem, setDragItem] = useState({})
-  const [dropItem, setDropItem] = useState({})
+  const [dragItem, setDragItem] = useState({});
+  const [dropItem, setDropItem] = useState({});
 
   const allowDrag = (event) => {
     event.preventDefault();
-  }
+  };
+
+  const dragNdrop = useCallback(() => {
+    const temp = list.splice(dragItem.index, 1);
+    list.splice(dropItem.index, 0, ...temp);
+    setList([...list]);
+    resetDragDrop();
+  }, [dragItem, dropItem, list]);
 
   useEffect(() => {
     if (dragItem.item && dropItem.item && dragItem.index !== dropItem.index) {
       dragNdrop();
-    } else if (dragItem.item && dropItem.item && dragItem.index === dropItem.index) {
+    } else if (dragItem.item && dropItem.item && dragItem.index === dropItem.index
+    ) {
       resetDragDrop();
     }
-  }, [dragItem, dropItem])
+  }, [dragItem, dropItem, dragNdrop]);
 
   const resetDragDrop = () => {
     setDragItem({});
     setDropItem({});
-  }
+  };
 
   const onDragStart = (item, index, event) => {
-    event.target.style.opacity = '0.4';
-    event.dataTransfer.effectAllowed = 'move';
-    event?.dataTransfer.setData('text/html', event?.target.innerHTML);
+    event.target.style.opacity = "0.4";
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.setData("text/html", event.target.innerHTML);
     setDragItem({ item, index });
-  }
+  };
 
   const onDragEnter = (event) => {
-    if(event.target.classList.contains('draggable')) {
-      event.target.classList.add('dragover');
+    if (event.target.classList.contains("draggable")) {
+      event.target.classList.add("dragover");
     }
-  }
+  };
 
   const onDragLeave = (event) => {
     event.stopPropagation();
-    event.target.classList.remove('dragover');
-  }
+    event.target.classList.remove("dragover");
+  };
 
   const onDragEnd = (event) => {
-    var listItems = document.querySelectorAll('.draggable');
-    [].forEach.call(listItems, function(item) {
-      item.classList.remove('dragover');
+    var listItems = document.querySelectorAll(".draggable");
+    [].forEach.call(listItems, function (item) {
+      item.classList.remove("dragover");
     });
-    event.target.style.opacity = '1';
-  }
+    event.target.style.opacity = "1";
+  };
 
   const onDrop = (item, index) => {
     setDropItem({ item, index });
-  }
+  };
 
-  const dragNdrop = () => {
-    const temp = list.splice(dragItem.index, 1);
-    list.splice(dropItem.index, 0, ...temp);
-    setList([...list])
-    resetDragDrop();
-  }
 
   return (
     <div className="listWrapper" onDragOver={allowDrag}>
-      {list.map((item, index) =>
+      {list.map((item, index) => (
         <div
           key={item.id}
           className="draggable"
@@ -81,8 +81,10 @@ function DraggableContainer({ children }) {
           onDrop={() => onDrop(item, index)}
           id={item.id}
           draggable={true}
-        >{item}</div>
-      )}
+        >
+          {item}
+        </div>
+      ))}
     </div>
   );
 }
